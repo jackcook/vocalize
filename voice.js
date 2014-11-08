f2fe41edf9a2083e89d1401133d9e5371f63cbc9
Jack Cook <jack@jackcook.us>
2014-11-08 16:29:35 -0500

    Fixed voice code to work with ace editor

--- voice.js
+++ voice.js
@@ -22,23 +22,17 @@ if (!!window.speechRecognition) {
 
 function process_command(command) {
   var args = command.split(" ");
-  var editor = document.getElementById("editor");
 
   if (args[0].substr(args[0].length - 1) == ".") {
-    editor.innerHTML += args[0] + args[1] + "()";
+    editor.insert(args[0] + args[1] + "()");
   } else {
     switch (args[0]) {
       case "break":
-        editor.innerHTML += "break\ntest";
+        editor.insert("break\ntest");
       case "call":
-        editor.innerHTML += args[3] + "." + args[1] + "()";
+        editor.insert(args[3] + "." + args[1] + "()");
       case "import":
-        editor.innerHTML += "import " + args[1];
+        editor.insert("import " + args[1]);
     }
   }
-}
-
-function write_line(line) {
-  var editor = document.getElementById("editor");
-  editor.innerHTML += line;
 }
