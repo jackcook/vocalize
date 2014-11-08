27c7718e06dd41d4808396fb2380ae2bdcbf40d9
Jack Cook <jack@jackcook.us>
2014-11-08 15:30:59 -0500

    Added basic commands system

--- voice.js
+++ voice.js
@@ -1,25 +1,41 @@
-window.speechRecognition = ( window.speechRecognition || window.webkitSpeechRecognition);
+window.speechRecognition = (window.speechRecognition || window.webkitSpeechRecognition);
 
 if (!!window.speechRecognition) {
-    recognition = new webkitSpeechRecognition();
-    recognition.continuous = true;
-    recognition.interimResults = true;
+  recognition = new webkitSpeechRecognition();
+  recognition.continuous = true;
+  recognition.interimResults = true;
+  recognition.start();
 
-    recognition.lang = "en-GB";
-    recognition.start();
-
-    recognition.onstart = function() { console.log("Speech recognition started."); }
-    recognition.onresult = function(event) {
-      for (var i = event.resultIndex; i < event.results.length; i++) {
-        if (event.results[i].isFinal) {
-          //console.log(event.results[i][0].transcript);
-        } else {
-          console.log(event.results[i][0].transcript);
-        }
+  recognition.onstart = function() { console.log("Speech recognition started."); }
+  recognition.onresult = function(event) {
+    for (var i = event.resultIndex; i < event.results.length; i++) {
+      if (event.results[i].isFinal) {
+        process_command(event.results[i][0].transcript);
       }
     }
-    recognition.onerror = function(event) { console.log("Error", event); }
-    recognition.onend = function() { console.log("Speech recognition ended"); }
+  }
+  recognition.onerror = function(event) { console.log("Error", event); }
+  recognition.onend = function() { console.log("Speech recognition ended"); }
 } else {
-    console.error("Speech recognition is not available on this device.");
+  console.error("Speech recognition is not available on this device.");
+}
+
+function process_command(command) {
+  var args = command.split(" ");
+  var editor = document.getElementById("editor");
+
+  if (args[0].substr(args[0].length - 1) == ".") {
+    editor.innerHTML += args[0] + args[1] + "()";
+  } else {
+    switch (args[0]) {
+      case "break":
+        editor.innerHTML += "break\ntest";
+      case "call":
+        editor.innerHTML += args[3] + "." + args[1] + "()";
+      case "test":
+        console.log(args[1]);
+      default:
+        console.log(args[0]);
+    }
+  }
 }
