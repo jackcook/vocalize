646427cbb7a04c89c09f8737c53afbca10c50ed5
Jack Cook <jack@jackcook.us>
2014-11-08 13:58:33 -0500

    Testing webkitSpeechRecognition

--- voice.js
+++ voice.js
@@ -1,1 +1,20 @@
+window.speechRecognition = ( window.speechRecognition || window.webkitSpeechRecognition);
 
+// Check access to the speech recogntion object
+if(!!window.speechRecognition) {
+    recognition = new webkitSpeechRecognition();
+    recognition.continuous = true;
+    recognition.interimResults = true;
+
+    recognition.lang = "en-GB";
+    recognition.start();
+
+    recognition.onstart = function() { console.log("Speech recognition started."); }
+    recognition.onresult = function(event) {
+        // Do something with the result
+    }
+    recognition.onerror = function(event) { console.log("Error", event); }
+    recognition.onend = function() { console.log("Speech recognition ended"); }
+} else {
+    console.error("Speech recognition is not available on this device.");
+}
