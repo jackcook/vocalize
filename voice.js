ecb8c1511569b9ff8043ff9eeb54b6365c654a5b
Jack Cook <jack@jackcook.us>
2014-11-08 22:07:33 -0500

    Began to implement support for other languages

--- voice.js
+++ voice.js
@@ -20,6 +20,8 @@ if (!!window.speechRecognition) {
   console.error("Speech recognition is not available on this device.");
 }
 
+var language = "python";
+
 function process_command(command) {
   var args = command.split(" ");
 
@@ -37,9 +39,9 @@ function process_command(command) {
         break;
       case "call":
         if (args[2] == "from") {
-          editor.insert(args[3] + "." + args[1] + "()");
+          editor.insert(call(args[1], args[3]));
         } else {
-          editor.insert(args[1] + "()");
+          editor.insert(call(args[1], ""));
         }
         process_command("break");
         break;
@@ -49,18 +51,24 @@ function process_command(command) {
             var params = args.slice(5);
             if (params.length > 1) {
               params.splice(-2, 1);
-              editor.insert("def " + args[2] + "(" + params.join(", ") + "):");
+              editor.insert(func(args[2], params));
             } else {
-              editor.insert("def " + args[2] + "(" + args[5] + "):");
+              editor.insert(func(args[2], args[5]));
             }
           } else {
-            console.log(args[4]);
-            editor.insert("def " + args[2] + "():");
+            editor.insert(func(args[2], ""));
           }
         } else if (args[1] == "string") {
           editor.insert("\"");
         }
         process_command("break");
+        break;
+      case "delete":
+        if (args[2] == "line") {
+          editor.removeToLineStart();
+        } else if (args[2] == "file") {
+          editor.destroy();
+        }
         break;
       case "and":
       case "end":
@@ -71,33 +79,40 @@ function process_command(command) {
           editor.insert("\"");
         }
         break;
-      case "import":
-        editor.insert("import " + args[1]);
-        break;
-      case "set":
-        if (args[2] == "language") {
-          editor.getSession().setMode("ace/mode/" + args[4].toLowerCase());
-        }
-        break;
       case "find":
         editor.find(args[1]);
+        break;
+      case "for":
+        if (args[2] == "between") {
+          editor.insert("for " + args[1] + " in range(" + args[3] + ", " + args[5] + "):")
+        } else if (args[2] == "in") {
+          editor.insert("for " + args[1] + " in " + args[3] + ":")
+        }
+        process_command("break");
+        break;
+      case "delta":
+      case "goto":
+        editor.gotoLine(args[2]);
+        break;
+      case "import":
+        editor.insert("import " + args[1]);
         break;
       case "replace":
         editor.replace(args[1]);
         break;
-      case "delete":
-        if (args[2] == "line") {
-          editor.removeToLineStart();
-        } else if (args[2] == "file") {
-          editor.destroy();
+      case "set":
+        if (args[2] == "language") {
+          editor.getSession().setMode("ace/mode/" + args[4].toLowerCase());
+          language = args[4].toLowerCase();
         }
         break;
-      case "goto":
-      case "delta":
-        editor.gotoLine(args[2]);
+      case "switch":
+        editor.insert(args[0] + " " + args[1] + ":");
+        process_command("break");
         break;
       case "if":
       case "while":
+        var keyword = args[0];
         args.shift();
         var argstr = args.join(" ");
         argstr = argstr.replace("is ", "");
@@ -110,25 +125,45 @@ function process_command(command) {
         argstr = argstr.replace("more", ">");
         argstr = argstr.replace("greater", ">");
 
-        editor.insert(args[0] + " " + argstr + ":");
-        process_command("break");
-        break;
-      case "switch":
-        editor.insert(args[0] + " " + args[1] + ":");
-        process_command("break");
-        break;
-      case "for":
-        if (args[2] == "between") {
-          editor.insert("for " + args[1] + " in range(" + args[3] + ", " + args[5] + "):")
-        } else if (args[2] == "in") {
-          editor.insert("for " + args[1] + " in " + args[3] + ":")
-        } else {
-          console.log(args);
-        }
+        editor.insert(ifwhile(keyword, argstr));
         process_command("break");
         break;
       default:
         console.log(args);
     }
+  }
+}
+
+function call(method, library) {
+  if (library == "") {
+    return method + "()";
+  } else {
+    return library + "." + method + "()";
+  }
+}
+
+function func(name, parameters) {
+  switch (language) {
+    case "python":
+      if (typeof parameters == "string") {
+        if (parameters == "") {
+          return "def " + name + "():";
+        } else {
+          return "def " + name + "(" + parameters + "):";
+        }
+      } else {
+        return "def " + name + "(" + parameters.join(", ") + "):";
+      }
+  }
+}
+
+function ifwhile(keyword, args) {
+  switch (language) {
+    case "javascript":
+      return keyword + " (" + args + ") {";
+    case "python":
+      return keyword + " " + args + ":";
+    case "swift":
+      return keyword + " " + args + " {";
   }
 }
